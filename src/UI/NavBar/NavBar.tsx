9f96d69848295e38fd87857f53a3a8202afa274d
Alex <alexgrigoriev.3614@gmail.com>
2024-04-13 22:27:23 +0300

    navbarfix

--- src/UI/NavBar/NavBar.tsx
+++ src/UI/NavBar/NavBar.tsx
@@ -1,19 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./NavBar.module.css";
 import { routes } from "../../router/routes.tsx";
 
 const NavBar = () => {
+    const router = useNavigate();
+
+    const goToMainPage = () => {
+        router("/");
+    };
+
     return (
         <div className={styles.navbar}>
-            <div className={styles.breadcrumbs}>
-                {routes.map((item, index) => (
-                    <Link className={styles.link} to={item.path} key={index}>
-                        <div className={styles.navitem}>
-                            <div className={styles.icon}>{item.icon}</div>
-                            <div>{item.body}</div>
-                        </div>
-                    </Link>
-                ))}
+            <div className={styles.navbar__container}>
+                <div onClick={goToMainPage} className={styles.logo}>
+                    LOGO
+                </div>
+                <div className={styles.links}>
+                    {routes.map((item, index) => (
+                        <Link
+                            className={styles.link}
+                            to={item.path}
+                            key={index}
+                        >
+                            <div className={styles.navitem}>
+                                <div className={styles.icon}>{item.icon}</div>
+                                <div>{item.body}</div>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
     );

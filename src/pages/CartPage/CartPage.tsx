9b0f4ddf595c361229121645312e0be8eea695c4
Alex <alexgrigoriev.3614@gmail.com>
2024-05-05 14:03:43 +0300

    cart and items fix

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -7,28 +7,44 @@ import { FC, useEffect, useState } from "react";
 const CartPage: FC = () => {
     const cart = useSelector((state: RootState) => state.market);
     const [totalSum, setTotalSum] = useState<number>(0);
+    const [itemsLength, setItemsLength] = useState<number>(0);
+    console.log(cart.length);
 
     useEffect(() => {
+        const itemsLength = cart.reduce(
+            (total, item) => total + item.counter,
+            0
+        );
         const total = cart.reduce(
             (total, item) => total + item.counter * (item.data.price * 93),
             0
         );
+        setItemsLength(itemsLength);
         setTotalSum(total);
     }, [cart]);
 
     return (
         <div className={styles.container}>
-            <div className={styles.page}>
-                <div className={styles.positions}>
-                    {cart.map((item, index) => (
-                        <ItemPosition key={index} item={item.data} />
-                    ))}
+            {!cart.length ? (
+                <div className={styles.total_sum}>Корзина пуста!</div>
+            ) : (
+                <div className={styles.page}>
+                    <div className={styles.positions}>
+                        {cart.map((item, index) => (
+                            <ItemPosition key={index} item={item.data} />
+                        ))}
+                    </div>
+                    <div className={styles.order}>
+                        <button className={styles.order_button}>
+                            Заказать!
+                            <div className={styles.total_sum}>{totalSum} ₽</div>
+                        </button>
+                        <div className={styles.total_items}>
+                            Товары, {itemsLength} шт
+                        </div>
+                    </div>
                 </div>
-                <div className={styles.order}>
-                    <button className={styles.order_button}>Заказать!</button>
-                    <div className={styles.total_sum}>{totalSum} ₽</div>
-                </div>
-            </div>
+            )}
         </div>
     );
 };
